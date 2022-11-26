d0d77173b222685929da7560cf936301c323c272
Luke Harris <luke+git@lkhrs.com>
2022-11-25 19:05:13 -0600

    W5P1 completed

---
+++ seven-seas/index.js
@@ -0,0 +1,10 @@
+//
+// Libraries
+//
+
+import { lootListeners, createTreasure, showLoot } from "./modules/lootOps.js";
+import "./modules/rollDice.js";
+
+createTreasure();
+showLoot();
+lootListeners();
